aa3ca6b0e0bf8550aba7cfb3ae1bbd853e6f040e
Leandro-Frota <leandromfrota@gmail.com>
2024-05-08 17:28:54 -0300

    Conect api and database mongoDb

--- src/app.js
+++ src/app.js
@@ -1,32 +1,29 @@
 import express from "express"
+import conectaNaDataBase from "./config/dbconect.js"
+import livro from "./models/Livros.js"
+
+const conexao = await conectaNaDataBase();
+
+conexao.on("Erro", (erro)=>{
+    console.error("erro na conexão",erro)
+});
+
+conexao.once("open",()=>{
+    console.log("Conexao com o banco feita com sucesso")
+} )
 
 const app = express()
 
 app.use(express.json())
 
-const livros = [
-    {
-        id:1,
-        titulo:"O senhor dos aneis"
-    },
-    {
-        id:2,
-        titulo:"O Hobbit"
-    }
-]
-
-function buscaLivro(id){
-    return livros.findIndex(livro =>{
-        return livro.id === Number(id)
-    })
-}
 
 app.get("/", (req,res)=>{
     res.status(200).send("Curso de Node.js");
 });
 
-app.get("/livros", (req,res) => {
-    res.status(200).json(livros)
+app.get("/livros", async (req,res) => {
+    const listaLivros = await livro.find({})    
+    res.status(200).json(listaLivros)
 })
 
 app.get("/livros/:id", (req,res)=>{
@@ -54,3 +51,4 @@ app.delete('/livros/:id', (req, res) => {
 
 
 export default app
+
